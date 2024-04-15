a6842ed167fd7d87040ac069b8062e4f385c15a5
Vasyl Mokhnatchuk <vasyl2756.com>
2024-04-15 22:10:41 +0300

    New router Car

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var githubRouter = require('./routes/github');
 var kypuSlonaRouter = require('./routes/kypu-slona');
+var carRouter = require('./routes/car')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/repositories', githubRouter);
 app.use('/kypu-slona', kypuSlonaRouter);
+app.use('/car', carRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
