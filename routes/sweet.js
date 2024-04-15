f6227dcbd35a1d8589fce283a8b511b28ff66a36
Maria19Sm <144223202+Maria19Sm@users.noreply.github.com>
2024-04-15 23:38:00 +0300

    Update sweet.js

--- routes/sweet.js
+++ routes/sweet.js
@@ -1,1 +1,13 @@
+var express = require('express');
+var router = express.Router();
 
+
+router.get('/', function(req, res, next) {
+  res.json({
+    name: 'Gummy',
+    from: 'Italy',
+    rate: 'the best',
+  });
+});
+
+module.exports = router;
